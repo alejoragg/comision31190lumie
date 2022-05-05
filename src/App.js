7cd48a95998d3942028f8edbf45409ede5ee1e11
Sebastian Zuviria <szuviria@gmail.com>
2022-05-04 22:35:06 -0300

    Clase de instalacion y configuracion del entorno

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+
+const AvatarJSX = (props) => {
+  console.log(props)
+  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
+
+  return (
+      <picture>
+          <img src={src} alt='Lego 1'/>
+          { props.name}
+      </picture>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AvatarJSX id={2} name={"Seba"}/>
+        <AvatarJSX id={3} name={"Alejandro"}/>
+        <AvatarJSX id={5} name={"Nicolas"}/>
       </header>
     </div>
   );
