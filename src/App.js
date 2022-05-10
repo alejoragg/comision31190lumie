847753832291b02f460daea72f99891ba3b9e9d4
Sebastian Zuviria <szuviria@gmail.com>
2022-05-09 22:33:14 -0300

    Clase de jsx-webpack

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
-const AvatarJSX = (props) => {
-  console.log(props)
-  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
-
-  return (
-      <picture>
-          <img src={src} alt='Lego 1'/>
-          { props.name}
-      </picture>
-  )
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <AvatarJSX id={2} name={"Seba"}/>
-        <AvatarJSX id={3} name={"Alejandro"}/>
-        <AvatarJSX id={5} name={"Nicolas"}/>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>ECommerce</h1>
+            <div>
+                <button>Celulares</button>
+                <button>Tablet</button>
+                <button>Notebook</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
