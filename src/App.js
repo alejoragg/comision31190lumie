20ddd515d3e2df6da03e58aa3a205337ed7f725a
Sebastian Zuviria <szuviria@gmail.com>
2022-05-23 22:37:54 -0300

    Clase de apis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
+// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import ItemCount from './components/ItemCount/ItemCount';
+import MercadoLibre from './components/MercadoLibre/MercadoLibre';
 
 const App = () => {
   return (
     <div className="App">
         <NavBar />
-        <ItemListContainer greeting="Hola Coders"/>
-        <ItemCount stock={10}/>
+        {/* <ItemListContainer greeting="Hola Coders"/>
+        <ItemCount stock={10}/> */}
+        <MercadoLibre />
     </div>
   );
 }
